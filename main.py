3cbd2ab9b3bab4304534483384aea42d6b1ba7bb
dhyey-dadhaniya <dhyeydadhania31@gmail.com>
2026-04-03 18:23:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import os
 
 app = FastAPI()
 
@@ -26,3 +27,8 @@ def predict(data: TransactionData):
         "label": "FRAUD" if prediction[0] == 1 else "NOT FRAUD",
         "fraud_probability": round(float(prob), 4)
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
